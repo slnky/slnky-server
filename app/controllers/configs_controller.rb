e0ff4db24077382f5547535575fecba718a52a89
Shawn Catanzarite <me@shawncatz.com>
2016-03-28 16:30:02 -0700

    use dup to avoid overriding values in secrets

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -1,11 +1,13 @@
 class ConfigsController < ApplicationController
+  before_filter :set_cache_headers
+
   def index
     # ?
   end
 
   def show
     name = params['id']
-    config = secrets['common']
+    config = secrets['common'].dup
     config[name] = secrets[name] if secrets[name]
     render json: config
   end
@@ -14,5 +16,13 @@ class ConfigsController < ApplicationController
 
   def secrets
     Rails.application.secrets
+  end
+
+  private
+
+  def set_cache_headers
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
